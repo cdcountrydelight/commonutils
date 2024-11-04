27c3819df988c9c92a2e111c708d8ac9d52d8d2e
Arpit Katiyar <arpitkatiyar@countrydelight.in>
2024-11-04 21:21:27 +0530

    updated file

--- cd-utility/src/main/java/com/cd/utility/localstorage/PreferenceDataStore.kt
+++ cd-utility/src/main/java/com/cd/utility/localstorage/PreferenceDataStore.kt
@@ -1,21 +1,34 @@
 package com.cd.utility.localstorage
 
 import android.content.Context
+import androidx.datastore.core.DataMigration
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 
 class PreferenceDataStore(
-    private val context: Context,
-    private val preferenceName: String = "delivery_app"
+    context: Context,
+    name: String = "delivery_app",
+    corruptionHandler: ReplaceFileCorruptionHandler<Preferences>? = null,
+    produceMigrations: (Context) -> List<DataMigration<Preferences>> = { listOf() },
+    scope: CoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 ) : IPreferenceDataStoreAPI {
 
-    val Context.datastore: DataStore<Preferences> by preferencesDataStore(name = preferenceName)
+    val Context.datastore: DataStore<Preferences> by preferencesDataStore(
+        name = name,
+        corruptionHandler = corruptionHandler,
+        produceMigrations = produceMigrations,
+        scope = scope
+    )
     val dataSource = context.datastore
 
 
